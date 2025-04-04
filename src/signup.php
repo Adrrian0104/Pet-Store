0c4dffe148e46b119bdb81332658bc43f3b0a851
Adrian <adrianconstainb@gmail.com>
2025-04-03 20:28:25 -0500

    Error

--- src/signup.php
+++ src/signup.php
@@ -4,15 +4,35 @@
     $lname=$_POST['l_name'];
     $email=$_POST['e_mail'];
     $passw=$_POST['p_assw'];
-    $sql= "INSERT INTO users
-            (firstname, lastname, email, password)
-            values ('$fname','$lname','$email','$passw')
+
+    $sql_validate_email="
+            select count(id)
+         from users
+         where email='$email'and 
+         status= true;
 
     ";
-    $ans = pg_query($conn, $sql);
+    $ans = pg_query($conn, $sql_validate_email);
     if($ans){//$ans==true
-        echo " user has been created successfully";
+        $row=pg_fetch_assoc($ans);
+        if($row['total']>0){
+            echo "User already exist !!!";
+
+        }else{
+            $sql ="INSERT INTO user 
+            (firstname, lastname, email, password)
+            VALUES ('$fname','$lname','$email','$passw')
+            ";
+            $ans = pg_query($conn,$sql);
+            if($ans){
+                echo "User has been created successfully";
+            }else{
+                echo "Error";
+            }
+        }
     }else{
-        echo "Error";
+        echo "Query error";
     }
+
+   
 ?>
